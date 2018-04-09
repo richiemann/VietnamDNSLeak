1f6b1a6c0edb255dc93d8037c970a4e601f19203
Ricardo Gandara <ricardo@amisus.co.uk>
2018-04-09 03:04:47 +0000

    Fix output to only display highest priority nameserver

--- get_ns_records_parallel.sh
+++ get_ns_records_parallel.sh
@@ -5,7 +5,7 @@ for i in *vn
 	do 
 		cd "$DIR"/$i; 
 		rm $i.mailservers 2>/dev/null
-		cat $i.domains | xargs -I {}  -P 512 -n 1 sh -c 'mailserver=`dig +short MX {} | sort -n | cut -f 2 -d " "`; echo {},$mailserver' >> $i.mailservers
+		cat $i.domains | xargs -I {}  -P 2048 -n 1 sh -c 'mailserver=`dig +short MX {} | sort -n | head -1 |cut -f 2 -d " "`; echo {},$mailserver' >> $i.mailservers
 		NUMSERVERS=`wc -l $i.mailservers  | cut -f 1 -d " "`
 		echo "Zone $i: $NUMSERVERS Primary MX records"
 	done
